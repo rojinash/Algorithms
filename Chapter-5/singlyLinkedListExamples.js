3a9073ce9919ee3efe9cf169023b1cb8b9492b41
Rojina Sharma <sharmaro@grinnell.edu>
2019-10-08 17:28:31 -0700

    update SLL examples to include removing from the beginning and the end

--- Chapter-5/singlyLinkedListExamples.js
+++ Chapter-5/singlyLinkedListExamples.js
@@ -1,13 +1,13 @@
 function SLNode(val){
     this.value = val;
-    this.next = null;//a pointer to the next SLNode
+    this.next = null;//a pointer to the next SLNode(with the attributes value and next)
 }
 
 function SLL(){
     this.head = null;
+}
 
-
-this.addToFront = function(val){
+SLL.prototype.addToFront = function(val){
     var newNode = new SLNode(val);
     if(this.head==null){
         this.head = newNode;
@@ -17,7 +17,41 @@ this.addToFront = function(val){
     }
     return this;
 }
+
+SLL.prototype.addToEnd = function(val){
+    var newNode = new SLNode(val);
+    if(this.head==null){
+        this.head = newNode;
+    }else{
+        var current = this.head;
+        while(current.next!=null){
+            current=current.next
+        }
+        current.next=newNode;
+    }
+    return this;
 }
+
+SLL.prototype.removeFromFront = function(){
+    if(this.head==null){
+        return;
+    }
+    this.head = this.head.next;
+    return this;
+}
+
+SLL.prototype.removeFromEnd = function(){
+    if(!this.head){
+        return;
+    }
+    var current = this.head;
+    while(current.next.next!=null){
+        current = current.next;
+    }
+    current.next=null;
+    return this;
+}
+
 SLL.prototype.printLst = function(){
     var current = this.head;
     while(current.next!=null){
@@ -28,7 +62,11 @@ SLL.prototype.printLst = function(){
 }
 
 var myList = new SLL();
-myList.addToFront(9);
-myList.addToFront(6);
-
+myList.addToFront(9).addToFront(8).addToFront(7).addToFront(6).addToFront(5);
+myList.printLst();
+myList.addToEnd(1).addToEnd(2).addToEnd(3);
+myList.printLst();
+myList.removeFromFront();
+myList.printLst();
+myList.removeFromEnd();
 myList.printLst();
