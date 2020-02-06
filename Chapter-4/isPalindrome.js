cacdef176d3d19c7b08558703adb4acb13bb4bf8
Rojina Sharma <sharmaro@grinnell.edu>
2020-02-05 16:38:36 -0800

    add longest palindrome

--- Chapter-4/isPalindrome.js
+++ Chapter-4/isPalindrome.js
@@ -1,38 +1,55 @@
 //Does not ignore capitals, spaces, punctuation.
-function isPalindrome(str){
-    var first = 0, last = str.length-1;
-    while(first<last){
-      if(str[first]!=str[last]){
-        return false;
-      }
-      first++;
-      last--;
+function isPalindrome(str) {
+    var first = 0,
+        last = str.length - 1;
+    while (first < last) {
+        if (str[first] != str[last]) {
+            return false;
+        }
+        first++;
+        last--;
     }
     return true;
-  }
- 
-  //Ignores capitals, spaces, all punctuation.
-  function isPalindromeFancy(str){
+}
+
+//Ignores capitals, spaces, all punctuation.
+function isPalindromeFancy(str) {
     str = str.toLowerCase();
     var newStr = "";
-    for(var i = 0; i < str.length; i++){
-        if(str.charCodeAt(i)>96 && str.charCodeAt(i)<124){
-            newStr+= str[i];
+    for (var i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) > 96 && str.charCodeAt(i) < 124) {
+            newStr += str[i];
         }
     }
     return isPalindrome(newStr);
-  }
+}
 
-  function longestPalindrome(str){
+function longPali(str) {
+    let longest = "";
+    let max = 0;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; i < str.length + 1; i++) {
+            let sub_str = str.substring(i, j)
+            if (isPalindrome(sub_str) && sub_str.length > max) {
+                longest = sub_str;
+                max = longest.length;
+            }
+        }
+    }
+    return longest;
+}
+
+function longestPalindrome(str) {
     var longest = str[0];
-    for(var i = 0; i < str.length; i++){
-        for(var j = i+2; j<str.length+1; j++){
-            var cur = str.substring(i,j);
-            if(isPalindromeFancy(cur)&&(cur.length > longest.length)){
+    for (var i = 0; i < str.length; i++) {
+        for (var j = i + 2; j < str.length + 1; j++) {
+            var cur = str.substring(i, j);
+            if (isPalindromeFancy(cur) && (cur.length > longest.length)) {
                 longest = cur;
             }
         }
     }
     return longest;
 }
-console.log(longestPalindrome("Does this sentence have any palindromes? What's the longest I wonder..."));
+// console.log(longestPalindrome("Does this sentence have any palindromes? What's the longest I wonder..."));
+console.log(longPali("hello world this is "))
