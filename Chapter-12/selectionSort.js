ac358abaa339d9641281130ec164e6185b2e5810
Connor <cdubthecoolcat@gmail.com>
2020-02-06 18:34:34 -0800

    finish selection sort

--- Chapter-12/selectionSort.js
+++ Chapter-12/selectionSort.js
@@ -1,6 +1,19 @@
-function selectionSort(arr){
-    for(var i = 0; i < arr.length; i++){
-        var curMin = arr[i];
-        
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var curMinIndex = i;
+
+    for (var j = i; j < arr.length; ++j) {
+      if (arr[curMinIndex] > arr[j]) {
+        curMinIndex = j;
+      }
     }
+
+    const tmp = arr[i];
+    arr[i] = arr[curMinIndex];
+    arr[curMinIndex] = tmp;
+  }
 }
+
+let testArr = [19, 5, 6, 14, 67, 36, 17, 34];
+selectionSort(testArr);
+console.log(testArr);
